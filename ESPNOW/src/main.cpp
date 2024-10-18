82b2ad26d6c18335e3054d86944ea12bf1db6c62
ChristopherSuazo07 <cristophersuazo9@gmail.com>
2024-10-17 23:03:30 -0600

    Creacion de Sender y Receiver :fire:

--- ESPNOW/src/main.cpp
+++ ESPNOW/src/main.cpp
@@ -1,18 +1,44 @@
-#include <Arduino.h>
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <esp_wifi.h>
+  #include <Arduino.h>
+  #include "FS.h"
+  #include <WiFiClientSecure.h>
+  #include <Wire.h>
+  
 
-// put function declarations here:
-int myFunction(int, int);
+#else
+  #include <ESP8266WiFi.h>
+#endif
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+  Serial.begin(115200);
+
+  Serial.print("ESP Board MAC Address: ");
+  #ifdef ESP32
+    WiFi.mode(WIFI_STA);
+    uint8_t baseMac[6];
+    esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+    if (ret == ESP_OK) {
+      Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                    baseMac[0], baseMac[1], baseMac[2],
+                    baseMac[3], baseMac[4], baseMac[5]);
+    } else {
+      Serial.println("Failed to read MAC address");
+    }
+  #else
+    Serial.println(WiFi.macAddress());
+  #endif
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// MAC de Slave> c8:2e:18:f0:01:f0
+//MAC de la que esta en foami blanco> c8:2e:18:8f:00:f4 
+void loop(){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
